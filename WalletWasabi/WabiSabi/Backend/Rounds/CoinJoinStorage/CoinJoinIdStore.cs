f7dc05a50430cb4aea1439b20f653a75e12374c7
Szpoti <balage.farago@gmail.com>
2022-04-05 15:59:21 +0200

    Create CoinJoinIdStore

---
+++ WalletWasabi/WabiSabi/Backend/Rounds/CoinJoinStorage/CoinJoinIdStore.cs
@@ -0,0 +1,49 @@
+using NBitcoin;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WalletWasabi.Logging;
+
+namespace WalletWasabi.WabiSabi.Backend.Rounds.CoinJoinStorage;
+
+public class CoinJoinIdStore : ICoinJoinIdStore
+{
+	public InMemoryCoinJoinIdStore InMemoryCoinJoinIdStore { get; set; }
+	public string CoinJoinsFilePath { get; set; }
+	private object FileWriteLock { get; set; } = new();
+
+	public CoinJoinIdStore(string ww2CJFilePath)
+	{
+		CoinJoinsFilePath = ww2CJFilePath;
+		InMemoryCoinJoinIdStore = InMemoryCoinJoinIdStore.LoadFromFile(ww2CJFilePath);
+	}
+
+	public void Append(uint256 id)
+	{
+		InMemoryCoinJoinIdStore.Add(id);
+		try
+		{
+			lock (FileWriteLock)
+			{
+				File.AppendAllLines(CoinJoinsFilePath, new[] { id.ToString() });
+			}
+		}
+		catch (Exception ex)
+		{
+			Logger.LogError($"Could not write file {CoinJoinsFilePath}.", ex);
+		}
+	}
+
+	public bool Contains(uint256 id)
+	{
+		return InMemoryCoinJoinIdStore.Contains(id);
+	}
+
+	public void FetchOldCoinJoins(string coinJoinsFilePath, string coinJoinIdStoreFilePath)
+	{
+		InMemoryCoinJoinIdStore.ImportWW1CoinJoinsToWW2(coinJoinsFilePath, coinJoinIdStoreFilePath);
+	}
+}
